4bd00e9cfa010748218a7e25e8f52021c647f68f
Mustapha Ahmad <18-30gc052@students.unilorin.edu.ng>
2022-07-06 12:16:45 +0300

    setting up production

--- url_shortner/settings/production.py
+++ url_shortner/settings/production.py
@@ -1,3 +1,27 @@
 from . import base
+from decouple import config
 
 
+DATABASES = {
+  'default': {
+    'ENGINE': 'django_psdb_engine',
+    'NAME': config('DB_NAME'),
+    'HOST': config('DB_HOST'),
+    'PORT': config('DB_PORT'),
+    'USER': config('DB_USER'),
+    'PASSWORD': config('DB_PASSWORD'),
+    'OPTIONS': {'ssl': {'ca': config('MYSQL_ATTR_SSL_CA')}}
+  }
+}
+
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': config("REDIS_URL"),
+        'TIMEOUT': 30,
+        'KEY_PREFIX': "dev"
+    }
+}
+
+ALLOWED_HOSTS = ["134.122.124.219","*"]
