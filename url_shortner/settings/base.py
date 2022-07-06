3497fb07a85425efe98f476bd928bf80215cf2a6
Mustapha Ahmad <18-30gc052@students.unilorin.edu.ng>
2022-07-06 11:52:39 +0300

    Revert "Setting up production"
    This reverts commit e304d798a1c91c4a495a731aed07892b79867bcb.

--- url_shortner/settings/base.py
+++ url_shortner/settings/base.py
@@ -132,7 +132,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- url_shortner/settings/development.py
+++ url_shortner/settings/development.py
@@ -30,4 +30,4 @@ CACHES = {
     }
 }
 
-ALLOWED_HOSTS = ["134.122.124.219"]
+ALLOWED_HOSTS = []
