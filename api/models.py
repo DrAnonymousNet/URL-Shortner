8f55ab8211480cbaaac9b6c5b89192c2a8592479
Mustapha Ahmad <18-30gc052@students.unilorin.edu.ng>
2022-07-06 21:23:40 +0300

    migrations

--- api/models.py
+++ api/models.py
@@ -59,7 +59,7 @@ class AnalyticDateTimeManager(models.Manager):
 class Link(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, db_constraint=False)
     short_link = models.URLField(_("Short link"), editable=False)
-    long_link = models.TextField(_("Long link"), blank=False, null=False, validators=[URLValidator])
+    long_link = models.TextField(_("Long link"), blank=False, null=False)
     last_visited_date = models.DateField(_("last visited"), auto_now=True, editable=False)
     visit_count = models.PositiveBigIntegerField(_("visit count"),editable=False, default=0)
     date_created = models.DateField(auto_now_add=True)
