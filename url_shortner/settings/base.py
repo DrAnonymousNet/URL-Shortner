55ebef333571808fa065d40649d6a4808b9b9176
Mustapha Ahmad <18-30gc052@students.unilorin.edu.ng>
2022-07-07 22:46:02 +0300

    CR

--- url_shortner/settings/base.py
+++ url_shortner/settings/base.py
@@ -239,6 +239,18 @@ LOGGING = {
 
 CORS_ALLOWED_ORIGINS = True
 
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
 CORS_ALLOW_METHODS = [
     "DELETE",
     "GET",
