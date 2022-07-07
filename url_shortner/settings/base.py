92fce0222d84c987af2bcba8dee94800b9172608
Mustapha Ahmad <18-30gc052@students.unilorin.edu.ng>
2022-07-07 22:30:01 +0300

    whitenoise

--- url_shortner/settings/base.py
+++ url_shortner/settings/base.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    #"whitenoise.middleware.WhiteNoiseMiddleware",
 
     'django.contrib.sessions.middleware.SessionMiddleware',
       "corsheaders.middleware.CorsMiddleware",
