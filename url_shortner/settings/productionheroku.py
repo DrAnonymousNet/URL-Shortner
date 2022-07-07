cfff61c0144214367c997af1e09aea054ec7d2ab
Mustapha Ahmad <18-30gc052@students.unilorin.edu.ng>
2022-07-08 00:11:36 +0300

    static

--- url_shortner/settings/productionheroku.py
+++ url_shortner/settings/productionheroku.py
@@ -29,7 +29,6 @@ CACHES = {
 
 DEBUG = config('DEBUG', False, cast=bool)
 ALLOWED_HOSTS = ["shortenr.herokuapp.com","*", "134.122.124.219"]
-MIDDLEWARE += ["whitenoise.middleware.WhiteNoiseMiddleware",]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" 
 #STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
