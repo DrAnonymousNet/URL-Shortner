668bf4d021d3fbba8c7a7564bb9041d4ee8b12b7
Mustapha Ahmad <18-30gc052@students.unilorin.edu.ng>
2022-07-21 20:06:25 +0100

    Reduced Access Token Time

--- url_shortner/settings/base.py
+++ url_shortner/settings/base.py
@@ -169,7 +169,7 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=2),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
    'AUTH_HEADER_TYPES': ('Bearer',),
    'BLACKLIST_AFTER_ROTATION':False
