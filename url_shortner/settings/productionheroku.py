3c8dd2a2464f06b4d7f44ffa276a65711887022d
Mustapha Ahmad <18-30gc052@students.unilorin.edu.ng>
2022-07-31 21:07:47 +0100

    settings

--- url_shortner/settings/productionheroku.py
+++ url_shortner/settings/productionheroku.py
@@ -11,8 +11,8 @@ DATABASES = {
     'PORT': config('DB_PORT'),
     'USER': config('DB_USER'),
     'PASSWORD': config('DB_PASSWORD'),
-    #'OPTIONS': {'ssl': {'ca': os.environ.get('MYSQL_ATTR_SSL_CA')}}
-    'OPTIONS': {'ssl': False}
+    'OPTIONS': {'ssl': {'ca': os.environ.get('MYSQL_ATTR_SSL_CA')}}
+    #'OPTIONS': {'ssl': False}
 
   }
 }
